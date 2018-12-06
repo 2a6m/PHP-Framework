fac2867db657fd4025beb98567c5a35e43d6e121
2a6m <maxime.bourguignon1996@gmail.com>
2018-12-06 14:19:42 +0100

    create components artist, music

--- Angular/my-app/src/app/app.module.ts
+++ Angular/my-app/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MusicComponent } from './music/music.component';
+import { ArtistComponent } from './artist/artist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicComponent,
+    ArtistComponent
   ],
   imports: [
     BrowserModule,
